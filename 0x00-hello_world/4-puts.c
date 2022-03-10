4d47a21ab2e440f4fcb7d4b62fdee60b7a562e13
Zoe Stocker <stockerazoe@gmail.com>
2022-03-10 09:10:46 -0800

    Changing the block in the file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,7 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
