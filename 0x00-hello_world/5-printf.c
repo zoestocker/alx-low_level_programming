b82172169e3c0c4f6ab8131af78748fabe2aef56
Zoe Stocker <stockerazoe@gmail.com>
2022-03-10 10:21:40 -0800

    Adding a comment to 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Using printf to display the string
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
