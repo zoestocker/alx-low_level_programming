7858058237d0524faab88e714684bf67b4c810a3
Zoe Stocker <stockerazoe@gmail.com>
2022-03-11 11:44:45 -0800

    Using putchar twice to print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Printing single digit numbers with putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
