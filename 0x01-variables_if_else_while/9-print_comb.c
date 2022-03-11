5ff1d392322eb72eaf8d70eea95f7277350013bd
Zoe Stocker <stockerazoe@gmail.com>
2022-03-11 13:17:03 -0800

    Printing a list of numbers with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Printing a list of integers with commas
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	if (c != '9')
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
