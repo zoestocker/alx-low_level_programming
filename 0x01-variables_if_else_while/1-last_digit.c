cf6137cda91ffe58b5b2280e7b3502697829f199
Zoe Stocker <stockerazoe@gmail.com>
2022-03-11 05:22:42 -0800

    Adding a condition

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	if (n == 0)
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	if (0 < n < 6)
+	if (n != 0 && n < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	return (0);
 }
