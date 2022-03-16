d6201421f4a757f6a464848537b2d757d43b3673
Zoe Stocker <stockerazoe@gmail.com>
2022-03-16 04:42:05 -0700

    Printing the sign in front of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - prints the sign of a number.
+ * Description: print_sign prints the sign on n.
+ * @n: number whose sign is being printed.
+ * Return: 1 if n is greater than zero,
+ * -1 if less than zero, 0 if equal to zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n >= 0 && n < 1)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
