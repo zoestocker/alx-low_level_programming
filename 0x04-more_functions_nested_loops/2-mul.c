9fd6c17a089e7a0d6eea5937b748cee3370e21ae
Zoe Stocker <stockerazoe@gmail.com>
2022-03-17 12:32:38 -0700

    Print the numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: c
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	if (a < '0' && a >= '0')
+	if (b < '0' && b >= '0')
+	return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - prints the numbers
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
