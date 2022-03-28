ea845e443c0bdce621dd1927b1f948f458facb33
Zoe Stocker <stockerazoe@gmail.com>
2022-03-28 05:54:37 -0700

    How to update the value in a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates its value to 98
+ * @n: the integer the pointer is pointing to
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning how to use pointers, arrays and strings
