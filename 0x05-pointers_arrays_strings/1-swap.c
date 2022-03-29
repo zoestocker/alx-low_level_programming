a6b855beb058b5d6836333367a837b800c8a8896
Zoe Stocker <stockerazoe@gmail.com>
2022-03-29 03:47:05 -0700

    Printing a string with a newline

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,6 +3,8 @@
 
 /**
  * swap_int - swaps values of two integers
+ * @a: The first integer
+ * @b: The second integer
  */
 void swap_int(int *a, int *b)
 {
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a newline to stdout
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
