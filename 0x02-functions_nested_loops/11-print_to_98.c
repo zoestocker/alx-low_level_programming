7b0ddad27fd2de01b3617ea5a24e0b6200513d70
Zoe Stocker <stockerazoe@gmail.com>
2022-03-31 05:36:40 -0700

    Updating the main branch

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
  */
 void print_to_98(int n)
 {
-	if (n 
+	for (n )
+	{
+		printf("%d ", n);
+	}
+	_putchar('\n');
+	return;
+}
