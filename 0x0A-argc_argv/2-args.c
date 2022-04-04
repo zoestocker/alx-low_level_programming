862313155041ce4ec59eec91d88797b58e2d19b4
Zoe Stocker <stockerazoe@gmail.com>
2022-04-04 08:29:39 -0700

    How to print all the arguments a program receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all the arguments it receives
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
