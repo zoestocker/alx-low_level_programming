aa2e889e70d2e0546dfa8e415480e36afbee613a
Zoe Stocker <stockerazoe@gmail.com>
2022-04-04 00:36:25 -0700

    Printing a chess board

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: the array
+ * @size: integer
+ */
+void print_diagsums(int **a, int size)
+{
+	int i, j, sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum = sum + a[i][j];
+			}
+		}
+	}
+	printf("%d", sum);
+}
