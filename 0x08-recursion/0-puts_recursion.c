95b46a986cede9f731a46b66cf796f9afc2a3eb3
Zoe Stocker <stockerazoe@gmail.com>
2022-04-04 03:57:47 -0700

    How to print a string without a loop

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		s = s + 1;
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
 }
